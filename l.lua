cbab45cb03640ce20d089bdb0aa49d8b9b698d90
nakinaka <79008607+nakinaka@users.noreply.github.com>
2022-04-16 20:28:33 +0800

    Update l.lua

--- l.lua
+++ l.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nakinaka/R6SIX/main/hexagon.lua", true))()
 
---loadstring(game:HttpGet("https://raw.githubusercontent.com/nakinaka/R6SIX/main/y57834t3b4tb893bg8959bh4985bh894hb845hyniokn65i95y", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nakinaka/R6SIX/main/s", true))()
